6d780b3eea2ddd75ade0b303985ee9935097b78c
Anmol Mittal <anmol0711mittal@gmail.com>
2024-01-13 19:11:05 +0530

    conditional rendering (if-else, ternary, with AND opeartor)

--- 18-21(Fragments, Maps, Conditional Rendering, Props, CSS Modules)/src/App.jsx
+++ 18-21(Fragments, Maps, Conditional Rendering, Props, CSS Modules)/src/App.jsx
@@ -1,13 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
-
-  let foodItems = ['Dal', "Green Vegetables", "Roti", "Salad", "Milk"]
+  let foodItems = []
+  // let foodItems = ['Dal', "Green Vegetables", "Roti", "Salad", "Milk"]
 
   return (
     // <> This is called React Fragments. We are using this to wrap our content and to prevent the use of unnecessary node
     <>
       <h1>Healthy Food</h1>
+      {foodItems.length === 0 && <h3>I am still hungry.</h3>}
       <ul className="list-group">
         {foodItems.map((item) => {
           return <li key={item} className="list-group-item">
