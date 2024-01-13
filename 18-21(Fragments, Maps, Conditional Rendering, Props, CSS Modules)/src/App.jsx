5b896dc498a32e3928b9464f7bc95f87dc831522
Anmol Mittal <anmol0711mittal@gmail.com>
2024-01-13 18:51:26 +0530

    use of fragments

--- 18-21(Fragments, Maps, Conditional Rendering, Props, CSS Modules)/src/App.jsx
+++ 18-21(Fragments, Maps, Conditional Rendering, Props, CSS Modules)/src/App.jsx
@@ -3,7 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css"
 function App() {
 
   return (
-    <div>
+    // <> This is called React Fragments. We are using this to wrap our content and to prevent the use of unnecessary node
+    <>
       <h1>Healthy Food</h1>
       <ul class="list-group">
         <li class="list-group-item">An item</li>
@@ -12,7 +13,7 @@ function App() {
         <li class="list-group-item">A fourth item</li>
         <li class="list-group-item">And a fifth one</li>
       </ul>
-    </div>
+    </>
   )
 }
 
