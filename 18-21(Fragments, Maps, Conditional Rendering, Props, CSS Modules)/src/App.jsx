549e2590af39ea344e682c97c0cc19006956ad1c
Anmol Mittal <anmol0711mittal@gmail.com>
2024-01-13 19:04:15 +0530

    use of map method

--- 18-21(Fragments, Maps, Conditional Rendering, Props, CSS Modules)/src/App.jsx
+++ 18-21(Fragments, Maps, Conditional Rendering, Props, CSS Modules)/src/App.jsx
@@ -2,19 +2,21 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
 
+  let foodItems = ['Dal', "Green Vegetables", "Roti", "Salad", "Milk"]
+
   return (
     // <> This is called React Fragments. We are using this to wrap our content and to prevent the use of unnecessary node
     <>
       <h1>Healthy Food</h1>
-      <ul class="list-group">
-        <li class="list-group-item">An item</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-        <li class="list-group-item">A fourth item</li>
-        <li class="list-group-item">And a fifth one</li>
+      <ul className="list-group">
+        {foodItems.map((item) => {
+          return <li key={item} className="list-group-item">
+            {item}
+          </li> // when we are using map we shoud define keys too with the values
+        })}
       </ul>
     </>
-  )
+  );
 }
 
 export default App
