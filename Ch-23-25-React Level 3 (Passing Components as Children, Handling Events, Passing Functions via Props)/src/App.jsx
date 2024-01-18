f38a14a70dcf1f6bc2a0303212239e084463275a
Anmol Mittal <anmol0711mittal@gmail.com>
2024-01-18 19:26:39 +0530

    concept of Container to pass children

--- Ch-23-25-React Level 3 (Passing Components as Children, Handling Events, Passing Functions via Props)/src/App.jsx
+++ Ch-23-25-React Level 3 (Passing Components as Children, Handling Events, Passing Functions via Props)/src/App.jsx
@@ -1,6 +1,7 @@
 
 import ErrorMessage from "./components/ErrorMessage";
 import FoodItems from "./components/FoodItems";
+import Container from "./components/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -10,12 +11,11 @@ function App() {
 
   return (
     // <> This is called React Fragments. We are using this to wrap our content and to prevent the use of unnecessary node
-    <>
+    <Container>
       <h1 className="food-heading" >Healthy Food</h1>
       <ErrorMessage foodItems={foodItems} ></ErrorMessage>
       <FoodItems foodItems={foodItems} ></FoodItems>
-
-    </>
+    </Container>
   );
 }
 
