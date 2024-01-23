5c65ca7c260b9d3db97c2dfc342408ab5a336e30
Anmol Mittal <anmol0711mittal@gmail.com>
2024-01-23 15:46:40 +0530

    concept of useEffect Hook

--- Ch-37-42-React Level 4 (Backend Dummy API, Data Fetching using Fetch function, useEffect Hook, Handling loading State, useEffect Hook Cleanup, Advanced useEffect)/src/components/PostList.jsx
+++ Ch-37-42-React Level 4 (Backend Dummy API, Data Fetching using Fetch function, useEffect Hook, Handling loading State, useEffect Hook Cleanup, Advanced useEffect)/src/components/PostList.jsx
@@ -1,23 +1,23 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
-  const handleGetPostsClick = () => {
+
+  // By use Effect we can reload the initial posts when the app starts
+  useEffect(() => {
     fetch("https://dummyjson.com/posts") // this is a dummy api from dummyjson.com
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts); // we are getting array of posts from above promise so we deifned a new object named as data and fetching the posts
       });
-  };
+  }, []); // this [] empty square brackets means we are fetching it for just one time when the app starts
 
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
-      )}
+      {postList.length === 0 && <WelcomeMessage />}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
--- Ch-37-42-React Level 4 (Backend Dummy API, Data Fetching using Fetch function, useEffect Hook, Handling loading State, useEffect Hook Cleanup, Advanced useEffect)/src/components/WelcomeMessage.jsx
+++ Ch-37-42-React Level 4 (Backend Dummy API, Data Fetching using Fetch function, useEffect Hook, Handling loading State, useEffect Hook Cleanup, Advanced useEffect)/src/components/WelcomeMessage.jsx
@@ -3,13 +3,6 @@ function WelcomeMessage({ onGetPostsClick }) {
   return (
     <center className="welcome-message">
       <h1>There are no Posts.</h1>
-      <button
-        type="button"
-        onClick={onGetPostsClick}
-        className="btn btn-primary"
-      >
-        Get posts from server
-      </button>
     </center>
   );
 }
