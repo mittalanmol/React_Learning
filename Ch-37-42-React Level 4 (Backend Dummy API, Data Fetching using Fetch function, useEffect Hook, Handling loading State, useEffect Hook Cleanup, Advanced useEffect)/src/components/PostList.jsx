6b2d6006165fec31a6150996636d0eb2a420b469
Anmol Mittal <anmol0711mittal@gmail.com>
2024-01-23 16:01:45 +0530

    Handled the Loading State

--- Ch-37-42-React Level 4 (Backend Dummy API, Data Fetching using Fetch function, useEffect Hook, Handling loading State, useEffect Hook Cleanup, Advanced useEffect)/src/components/PostList.jsx
+++ Ch-37-42-React Level 4 (Backend Dummy API, Data Fetching using Fetch function, useEffect Hook, Handling loading State, useEffect Hook Cleanup, Advanced useEffect)/src/components/PostList.jsx
@@ -1,26 +1,31 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
 
+  const [fetching, setFetching] = useState(false); // initialy fecthing is false, it means data aa chuka hai humm fetch nhi kr rhe
+  // in case of states we use []
+
   // By use Effect we can reload the initial posts when the app starts
   useEffect(() => {
+    setFetching(true); // true means humm fetch kr rhe hai
     fetch("https://dummyjson.com/posts") // this is a dummy api from dummyjson.com
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts); // we are getting array of posts from above promise so we deifned a new object named as data and fetching the posts
+        setFetching(false);
       });
   }, []); // this [] empty square brackets means we are fetching it for just one time when the app starts
 
   return (
     <>
-      {postList.length === 0 && <WelcomeMessage />}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 };
