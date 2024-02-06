02f106e231641b80ef7b3fa79e560610d70ff634
Anmol Mittal <anmol0711mittal@gmail.com>
2024-02-06 21:20:20 +0530

    created all the required components

--- Ch-52-56-Redux (Introduction, React-Context vs Redux, How Redux Works, Working with Redux, React with Redux)/src/App.jsx
+++ Ch-52-56-Redux (Introduction, React-Context vs Redux, How Redux Works, Working with Redux, React with Redux)/src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Controls from "./components/Controls";
+import DisplayCounter from "./components/DisplayCounter";
+import Header from "./components/Header";
+import { Children } from "react";
+import Container from "./components/Container";
 
 function App() {
-  return <></>;
+  return (
+    <center className="px-4 py-5 my-5 text-center">
+      <Container>
+        <Header></Header>
+
+        <DisplayCounter />
+        <Controls />
+      </Container>
+    </center>
+  );
 }
 
 export default App;
---
+++ Ch-52-56-Redux (Introduction, React-Context vs Redux, How Redux Works, Working with Redux, React with Redux)/src/components/DisplayCounter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DisplayCounter() {
+  return <p className="lead mb-4">Counter current Value: </p>;
+}
+
+export default DisplayCounter;
